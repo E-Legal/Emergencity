2eb0d8aec4b20692239ede79bacf7d57ef3ab661
Elliriah <fabien.hainaut@gmail.com>
2020-05-26 23:50:09 +0200

    Admin

---
+++ Web/src/views/Admin/Admin.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="admin">
+   <div class="container">
+      <div class="Chart">
+        <h2>Menu d'Administration</h2>
+            
+      </div>
+    </div>
+    <div class="container">
+      <div class="Chart">
+       <b-card no-body>
+      <b-tabs pills card vertical v-model="selected">
+        <!--- 0 -->
+       <b-tab title="Utilisateurs" lazy>
+          <User :total="this.users" />
+        </b-tab>
+        <!--- 1 -->
+        <b-tab title="Casernes" lazy>
+          <Barrack :total="this.barracks"/>
+        </b-tab>
+          <!--- 2 -->
+        <b-tab title="Feux de signalisation" lazy>
+          <Light :total="this.light"/>
+        </b-tab>
+      </b-tabs>
+     </b-card>
+      </div>
+    </div>
+  </div>
+      
+</template>
+
+<style scoped lang="scss">
+
+.loader {
+  border: 10px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 10px solid #3498db;
+  width: 30px;
+  margin: 0 auto;
+  height: 30px;
+  -webkit-animation: spin 2s linear infinite; /* Safari */
+  animation: spin 2s linear infinite;
+}
+
+.Chart {
+  background: #212733;
+  border-radius: 15px;
+  box-shadow: 0px 2px 15px rgba(25, 25, 25, 0.27);
+  margin:  25px 0;
+}
+
+.Chart h2 {
+  margin-top: 0;
+  padding: 15px 0;
+  color:  #ababab;
+  text-align: center;
+  border-bottom: 1px solid #323d54;
+}
+.container {
+  max-width: 1500px;
+}
+/* Safari */
+@-webkit-keyframes spin {
+  0% { -webkit-transform: rotate(0deg); }
+  100% { -webkit-transform: rotate(360deg); }
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+</style>
+<script>
+  import LayoutDefault from "../../layouts/LayoutDefault.vue";
+  import Barrack from "./components/Barrack.vue";
+  import Light from "./components/Light.vue";
+  import User from "./components/User.vue";
+  import axios from 'axios';
+  
+  export default {
+    name: "admin",
+    components: {
+      Barrack,
+      Light,
+      User
+    },
+    data() {
+      return {
+        users: null,
+        barracks: null,
+        light: null,
+        selected: 0
+      };
+    },
+    watch: {
+
+    	selected(value) {
+              console.log("test");
+      	if (value === 0) {
+          // Mettre requête Utilisateur
+          axios.get('http://x2021emergencity2490271133000.northeurope.cloudapp.azure.com:9000/SU/user/list?token=' + localStorage.getItem('token')).then(response => {
+              console.log("USERS=");
+              console.log(response.data['content'])
+              this.users = response.data['content']
+          });
+         }
+        	if (value === 1) {
+            // Mettre requête Caserne
+            axios.get('http://x2021emergencity2490271133000.northeurope.cloudapp.azure.com:9000/barracks?token=' + localStorage.getItem('token')).then(response => {
+              console.log("Barracks=")
+
+              this.barracks = response.data['content']
+              console.log(this.barracks);
+          });
+         }
+        	if (value === 2) {
+            // Mettre requête Feux de signalisation
+            axios.get('http://x2021emergencity2490271133000.northeurope.cloudapp.azure.com:9000/feu?token=' + localStorage.getItem('token')).then(response => {
+              this.light = response.data['content']
+          });
+         }
+      }
+    },
+    mounted() {
+       axios.get('http://x2021emergencity2490271133000.northeurope.cloudapp.azure.com:9000/SU/user/list?token=' + localStorage.getItem('token')).then(response => {
+              console.log(response.data)
+              this.users = response.data['content']
+          });
+      this.$emit("update:layout", LayoutDefault);
+    },
+    created() {
+      this.log = localStorage.login;
+      this.$emit("update:layout", LayoutDefault);
+    }
+  };
+</script>
---
+++ Web/src/views/Admin/components/User.vue
@@ -0,0 +1,204 @@
+/* eslint-disable */
+<template>
+  <div name="User">
+         
+<div>
+  <Barracksmodal :idBarracks="idBarracks" msg="yolo"/>
+<b-tabs content-class="mt-3" justified>
+    <b-tab  v-if="this.label" title="Gestion Utilisateurs">
+    <form id="demo" @submit.prevent="submit" novalidate>
+          <div>
+            <input
+              type="text"
+              id="login"
+              class="col-lg-3"
+              name="login"
+              placeholder="Identifiant"
+              v-model="email"
+            />
+            <input
+              type="text"
+              id="password"
+              class="col-lg-3"
+              name="login"
+              placeholder="Mot de passe"
+              v-model="password"
+              style="margin-left: 1%; margin-right: 1%"
+            />
+            <input type="submit" class="btn btn-outline-primary" value="Ajouter un utilisateur" />
+          </div>
+      </form>
+      <vuetable ref="vuetable"
+    :fields="fields"
+    :api-mode="false"
+    :data="this.label"
+  >
+    <template slot="rank_actions" slot-scope="props"> 
+      <div>
+            {{assignUserRole(props['rowIndex'], props['rowData'])}}
+         <b-form-select style="width: 200px" v-model="selected[props['rowIndex']]" :options="options"></b-form-select>
+      </div>
+    </template>
+  <template slot="actions" slot-scope="props"> 
+      <div>
+
+        <b-button :id="props['rowData'].id" v-on:click="showMsgBoxOne(props, props['rowData'].id)" variant="danger">Supprimer</b-button>
+      </div>
+    </template>
+    </vuetable>
+
+
+    </b-tab>
+    <b-tab v-else title="Gestion utilisateurs"><div class="loader"></div></b-tab>
+</b-tabs>
+</div>
+
+  </div>
+</template>
+
+<style scoped>
+.gmap {
+width: 1920px; height: calc(100vh - 66px)
+}
+/* Safari */*
+.loader {
+  border: 10px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 10px solid #3498db;
+  width: 30px;
+  margin: 0 auto;
+  height: 30px;
+  -webkit-animation: spin 2s linear infinite; /* Safari */
+  animation: spin 2s linear infinite;
+}
+
+@-webkit-keyframes spin {
+  0% { -webkit-transform: rotate(0deg); }
+  100% { -webkit-transform: rotate(360deg); }
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+
+
+</style>
+
+<script>
+import Vuetable from 'vuetable-2';
+import axios from 'axios';
+import Barracksmodal from "../../../components/BarracksModal.vue";
+import Display from "../../../components/MyModal.vue";
+
+export default {
+  name: "User",
+  components: {
+    Vuetable,
+    Barracksmodal,
+    Display
+  },
+  props: {
+      total: Array
+    },
+  data() {
+    return {
+        idBarracks: null,
+        email: null,
+        password: null,
+        selected: [],
+        options: [
+          { value: null, text: 'Please select an option', disabled: true },
+          { value: 'Admin', text: 'Admin' },
+          { value: 'User', text: 'User' },
+          { value: 'Algorithm', text: 'Algorithm', disabled: true }
+        ],
+        fields: [
+        {
+          name: 'name',
+          title: 'Utilisateur'
+        },
+      {
+          name: 'rank_actions',
+          title: 'Rang',
+          titleClass: 'center aligned',
+          dataClass: 'center aligned'
+        },
+        {
+          name: 'caserne',
+          title: 'Caserne'
+        },
+        {
+          name: 'actions',
+          title: 'Actions',
+          titleClass: 'center aligned',
+          dataClass: 'center aligned'
+        },
+      ],
+        label: null,
+          boxOne: '',
+    };
+  },
+  watch: { 
+      	total: function(newVal, oldVal) { // watch it
+        console.log('Prop changed: ', newVal, ' | was: ', oldVal);
+        if (this.total) {
+                this.total.forEach((value, index) => {
+                  this.label = this.total;
+                  console.log(this.label);
+            });
+          }
+      }
+  },
+  methods: {
+    //...
+    showMsgBoxOne(props, val) {
+        this.boxOne = ''
+        this.$bvModal.msgBoxConfirm('Etes-vous sûr de vouloir supprimer cet utilisateur?')
+          .then(value => {
+            //this.boxOne = value
+            if (value === true) {
+              console.log("erase")
+              //this.eraseLight(props, val)
+              this.idBarracks = val
+              this.$bvModal.show("barracks_modal")
+              this.$bvModal.show("my-modal")sss
+            }
+          })
+          .catch(err => {
+            // An error occurred
+          })
+      },
+    onAction (action, data, index) {
+      console.log('slot) action: ' + action, data.name, index)
+    },
+    eraseLight(props, val) {
+      console.log(val);
+      console.log(props);
+      axios.delete("http://x2021emergencity2490271133000.northeurope.cloudapp.azure.com:9000/SU/user/" + val + '?token=' + localStorage.getItem("token")).then((response) => {
+
+      })
+      this.label.splice(props['rowIndex'], 1);
+
+    },
+    assignUserRole(rowIndex, rowData) {
+      if (rowData.name === "test" || rowData.name === "fabien_test") {
+       this.selected[rowIndex] = 'Admin'
+      } else if (rowData.name === 'algo') {
+        this.selected[rowIndex] = 'Algorithm'
+      } else {
+        this.selected[rowIndex] = 'User'
+      }
+    },
+    submit() {
+      axios.post('http://x2021emergencity2490271133000.northeurope.cloudapp.azure.com:9000/register?name=' + this.email + '&password=' + this.password)
+        .then((response) => {
+           if (response.status === 200) {
+             this.$forceUpdate();
+           }
+        });
+    }
+  }
+};
+
+</script>
