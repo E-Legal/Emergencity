421a25edd39787afa65fd72b0cf1c0007a4a7a03
Eliott <Legal.Eliott@gmail.com>
2020-08-25 09:45:03 +0200

    Merge Login

--- MobilV2/app/src/main/java/com/example/mobilv2/ApiManager.java
+++ MobilV2/app/src/main/java/com/example/mobilv2/ApiManager.java
@@ -1,15 +1,27 @@
 package com.example.mobilv2;
 
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.HttpUrl;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class ApiManager extends OkHttpClient {
     private String _url = "http://192.168.1.16:9000";
+    private String _token = "";
+
 
     public String getToken() {
-        return ("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1ZjE1YjNlZTJmOWNhMjMyZDQ3Y2NkNzMiLCJpYXQiOjE1OTUyNjcwMjR9.WNvhOohwdZCSuLrYxoDU7iQqUYRIgYLgf1qm0ipcLZ4");
+        return (_token);
     }
 
     private static final ApiManager ourInstance = new ApiManager();
@@ -31,6 +43,38 @@ public class ApiManager extends OkHttpClient {
                 .addHeader("Authorization", "Bearer " + getToken())
                 .build();
         newCall(request).enqueue(callback);
+    }
+
+    public void login(String content, final Callback callback) {
+        MediaType JSON = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(JSON, content);
+        final Request request = new Request.Builder()
+                .url(_url + "/users/login")
+                .post(body)
+                .build();
+        newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                callback.onFailure(call, e);
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    try {
+                        JSONObject responseBody = new JSONObject(response.body().string());
+                        _token = responseBody.getString("token");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                callback.onResponse(call, response);
+            }
+        });
+    }
+
+    public void removeToken() {
+        _token = "";
     }
 
 }
--- MobilV2/app/src/main/java/com/example/mobilv2/MainActivity.java
+++ MobilV2/app/src/main/java/com/example/mobilv2/MainActivity.java
@@ -2,13 +2,9 @@ package com.example.mobilv2;
 
 import android.os.Bundle;
 
-import com.example.mobilv2.ui.login.LoginFragment;
-import com.example.mobilv2.ui.notifications.NotificationsFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
